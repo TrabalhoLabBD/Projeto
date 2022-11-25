a096492486853911d3055d1d9cf8afdf5cfb6289
Augusto Estevão Monte <augusto-estevao@hotmail.com>
2022-11-25 20:13:38 -0300

    Adicionado outras tabelas
    Adicionei outras tabelas porém temos que revisar o documento word, pois a tabela que criei do financeiro tem uma foreign key apontando para plano de saúde, porém não consegui identificar qual seria pois, só temos uma PRIMARY KEY na tabela e ela é INTEIRO.

--- Hospital.sql
+++ Hospital.sql
@@ -32,8 +32,45 @@ CREATE TABLE paciente (
         OR sexo_pac = 'outro'),
     CHECK (altura_pac > 0 AND altura_pac < 3),
     CHECK (peso_pac > 0 AND peso_pac < 600),
-    CONSTRAINT FK_PACIENTE FOREIGN KEY (id_pds)
-        REFERENCES plano_de_saude (id_pds)
+    CONSTRAINT FK_PACIENTE FOREIGN KEY (id_pds) REFERENCES plano_de_saude (id_pds)
 );
 
 -- arrumar a fk
+
+CREATE TABLE atendimento(
+    id_atd int(10),
+    nome_atd varchar(100) NOT NULL,
+    tipo_atd varchar(100) NOT NULL,
+    PRIMARY KEY (id_atd)
+);
+
+CREATE TABLE financeiro(
+    conv_acc varchar(500),
+    conv_pac varchar(100),
+    PRIMARY KEY(conv_pac)
+);
+
+
+CREATE TABLE medico (
+    nome_med varchar(100) NOT NULL,
+    crm_med int(9),
+    especializacao_med varchar(30) NOT NULL,
+    ala_med varchar(30) NOT NULL,
+    PRIMARY KEY(crm_med)
+);
+
+CREATE TABLE enfermeiro(
+    nome_enf varchar(100) NOT NULL,
+    coren_enf int(9),
+    ala_enf varchar(30) NOT NULL,
+    PRIMARY KEY(coren_enf)
+);
+
+CREATE TABLE estoque(
+    id_est int(4),
+    nome_est varchar(50) NOT NULL,
+    categoria_est varchar(30) NOT NULL,
+    quantidade_est int NOT NULL,
+    preco_prod float NOT NULL,
+    PRIMARY KEY(id_est)
+);
