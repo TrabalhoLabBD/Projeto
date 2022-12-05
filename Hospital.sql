cc04030c05e2db9ef2a015fbdeb9772dc1ddfc13
Mauro Sérgio <maurosergiocantuaria@gmail.com>
2022-12-05 10:23:31 -0300

    População das tabelas financeiro, paciente, equipamento, atendimento, enfermeiro, plano_de_saude

--- Hospital.sql
+++ Hospital.sql
@@ -1,11 +1,12 @@
 CREATE DATABASE hospital;
+-- DROP DATABASE hospital;
 
 USE hospital;
 
 CREATE TABLE paciente (
     id_pac INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     nome_pac VARCHAR(100) NOT NULL,
-    cpf_pac INT(11) NOT NULL UNIQUE,
+    cpf_pac CHAR(11) NOT NULL UNIQUE,
     idade_pac INT(3) NOT NULL,
     sexo_pac VARCHAR(9) NOT NULL,
     altura_pac FLOAT(1) NOT NULL,
@@ -97,3 +98,38 @@ CREATE TABLE administrativo (
     CONSTRAINT FOREIGN KEY (id_adm_pac)
         REFERENCES paciente (id_pac)
 );
+
+INSERT INTO financeiro VALUES (1, 7,1505,4873);
+INSERT INTO financeiro VALUES (2, 3,3030,3752);
+INSERT INTO financeiro VALUES (3, 4,1344,4768);
+INSERT INTO financeiro VALUES (4, 5,1974,4207);
+INSERT INTO financeiro VALUES (5,2,1956,1241);
+
+INSERT INTO paciente VALUES (0, "Marcelo", 19855448391, 19, "Masculino", 1.78, 70.00);
+INSERT INTO paciente VALUES (1, "Maicon", 20304448573, 30, "Masculino", 1.67, 69.25);
+INSERT INTO paciente VALUES (2, "André", 29384445038, 55, "Masculino", 1.77, 68.40);
+INSERT INTO paciente VALUES (3, "Luisa", 72938884736, 15, "Feminino", 1.64, 55.00);
+INSERT INTO paciente VALUES (6,"Marielly", 42039448574, 20, "Feminino", 1.60, 52.00);
+INSERT INTO paciente VALUES (5, "Mauro", 98598667423, 21, "Masculino", 1.79, 52.00);
+
+INSERT INTO equipamento VALUES (1, "Mascara", "tratamento");
+INSERT INTO equipamento VALUES (2, "Agulha", "diagnostico");
+INSERT INTO equipamento VALUES (3, "Maca", "suporte a vida");
+INSERT INTO equipamento VALUES (4, "Agulha", "diagnostico");
+INSERT INTO equipamento VALUES (5, "eletrocardiogrago", "diagnostico");
+
+INSERT INTO atendimento VALUES (1, "Traumatismo crâniano", "vermelho", "ALA 1");
+INSERT INTO atendimento VALUES (2, "torção no tornoselo", "verde", "ALA 2");
+INSERT INTO atendimento VALUES (3, "Dor de cabeça", "verde", "ALA 3");
+
+INSERT INTO enfermeiro VALUES (1,"Natasha",422124322, "",1);
+INSERT INTO enfermeiro VALUES (2,"Sasha",123342212,"" ,3);
+INSERT INTO enfermeiro VALUES (3,"Jennifer",324322324,"",2);
+
+INSERT INTO plano_de_saude VALUES (1,5,"VIDA",,,,);
+INSERT INTO plano_de_saude VALUES (2,4,"LIFE",,,,);
+INSERT INTO plano_de_saude VALUES (3,3,,,,,);
+INSERT INTO plano_de_saude VALUES (4,2,,,,,);
+INSERT INTO plano_de_saude VALUES (5,1,,,,,);
+
+SELECT * from paciente;	
